dbc512fced2ef1c5cc9222e15173110384b3fc77
Rudy Hardeman <zarya@gigafreak.net>
2012-04-29 02:10:16 +0200

    Added comments for the growler deamon

--- growler/growler.py
+++ growler/growler.py
@@ -1,9 +1,13 @@
+"""
+Author: Rudy Hardeman <zarya@gigafreak.net>
+"""
+
 from gntp import notifier
 import time
 import sys
 import Tac, PyClient, subprocess, os
 
- 
+#Tail file function (eats file handle)
 def tail_f(file):
   interval = 1.0
   file.seek( int(-1), 2 )
@@ -16,6 +20,7 @@ def tail_f(file):
     else:
       yield line
 
+#Build growl connection function
 def growl_init(config):
     growl = notifier.GrowlNotifier(
         applicationName = "Arista %s"%(hostname),
@@ -27,33 +32,47 @@ def growl_init(config):
     growl.register()
     return growl
 
+#Connect to internal config database
 pc = PyClient.PyClient("ar","Sysdb")
 root = pc.root()
 
+#Read out the switch hostname
 hostname = root['ar']['Sysdb']['sys']['net']['config'].hostname
 
+#Create the config file if it is not there
 _temp = open("/persist/sys/growl","a")
 _temp.close()
 
-#Apr 28 18:51:40 7048TOP Fru: %FRU-6-FAN_REMOVED: Fan tray 5 has been removed
-
+#Read all the lines comming from the logfile
 for line in tail_f(open("/var/log/messages")):
+    #Dont show log if there is only a cr on the line
     if line == "\n":
         continue
+
+    #Remove the timestamp from the log line
     line = line.split(" ",4)[4]
+
+    #Split in the service name and log entry
     (service,log) = line.split(":",1)
+
+    #If it is a default syslog set level to 6
     try:
         level = log.split("-")[1]
     except:
         level = "6"
+
+    #Open config for reading
     hosts = open("/persist/sys/growl","r")
     for _config in hosts:
         config = _config.split("\n")[0].split(",")
+        
+        #Try to connect to the growl host
         try:
             growl = growl_init(config)
         except:
             continue
         
+        #Send growl message
         growl.notify(
             noteType = "New Messages",
             title = "%s: %s"%(hostname,service),
